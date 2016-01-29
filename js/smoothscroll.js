bdab929b7b41925b998d073f0125be617fee52c3
Miguel Angel Flores Mejía <miguelflores.mkt@gmail.com>
2016-01-29 15:58:08 -0600

    SmoothScroll
    Se le añadió el smooth scroll

---
+++ js/smoothscroll.js
@@ -0,0 +1,14 @@
+<em>$(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
+   // prevent default anchor click behavior
+   e.preventDefault();
+   // store hash
+   var hash = this.hash;
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top
+     }, 300, function(){
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+});<br></em>
